d204a3b91e8fe7ebdc25c988dafb3f46a7c9a881
0H77 <105129744+0H77@users.noreply.github.com>
2024-12-12 02:17:48 +0400

    Registered models

--- sshsharedcart/cartapp/admin.py
+++ sshsharedcart/cartapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Profile, Supermarket, Product
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Supermarket)
+admin.site.register(Product)
