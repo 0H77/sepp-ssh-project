3d69d9bfbb0b7521a51c9a790fdd8b0e75f84a65
0H77 <105129744+0H77@users.noreply.github.com>
2024-12-12 07:03:21 +0400

    Refactor project structure: move requirements to sshsharedcart, update Dockerfile, rename university_name to address in Profile model, and add URL routing for cartapp

--- sshsharedcart/cartapp/models.py
+++ sshsharedcart/cartapp/models.py
@@ -7,7 +7,7 @@ from django.dispatch import receiver
 # Profile model
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    university_name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
 
     def __str__(self):
         return self.user.username
--- sshsharedcart/sshsharedcart/settings.py
+++ sshsharedcart/sshsharedcart/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -27,10 +26,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+LOGIN_REDIRECT_URL = '/supermarkets/'
 
 # Application definition
-
 INSTALLED_APPS = [
+    'cartapp',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'sshsharedcart.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'cartapp' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,7 +115,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "cartapp" / "templates" / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
